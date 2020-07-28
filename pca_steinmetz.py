47bf113fa4cfb3312018761b5f4e390eab8af6b4
laupso <53855889+laupso@users.noreply.github.com>
2020-07-28 20:12:02 +0200

    Update pca_steinmetz.py

--- pca_steinmetz.py
+++ pca_steinmetz.py
@@ -7,63 +7,6 @@ Created on Mon Jul 27 15:21:16 2020
 """
 
 
-def collect_spks(alldat, NT=92, balance_correct_incorrect=True, brain_areas='all'):
-    
-    import numpy as np
-    
-    spks = np.zeros([30000,NT,250])
-    
-    i0 = 0
-    
-    for dat in alldat:
-        
-        resp = dat['response']
-        stim = dat['contrast_left'] - dat['contrast_right']
-        stim = np.sign(stim) 
-        correct_   = resp == stim
-        incorrect_ = ~correct_
-        
-        min_NT = np.min([np.sum(correct_),np.sum(incorrect_)])
-        
-        if (balance_correct_incorrect) and (2*min_NT < NT):
-            # Don't take this session if number of trials is too small
-            continue
-       
-        active_neurons  = dat['spks'].sum(axis=2).mean(axis=1) > 1
-        if not brain_areas == 'all':
-            isin_brain_area = np.isin(dat['brain_area'], brain_areas)
-        else:
-            isin_brain_area = np.array(np.ones_like(dat['brain_area'])).astype(bool)
-        
-        spks_ = dat['spks'][active_neurons & isin_brain_area,:,:]
-        NN = spks_.shape[0]
-        
-        if balance_correct_incorrect:          
-            NTb = int(np.floor(NT/2))
-            idx1 = np.random.choice(np.array(np.where(correct_)).ravel(), NTb, replace = False)
-            idx2 = np.random.choice(np.array(np.where(incorrect_)).ravel(), NTb, replace = False)
-            idx  = np.concatenate([idx1,idx2])   
-        else:          
-            idx = np.random.choice(spks_.shape[0], NT, replace = False)
-            
-        
-        spks[i0:i0+NN,:,:] = spks_[:,idx,:]
-                
-        i0 = i0 + NN
-        
-    
-    spks = spks[:i0,:,:]
-    
-    if balance_correct_incorrect:
-        correct = np.zeros(NT)
-        correct[:NTb] = 1
-    else:
-        correct = None
-    
-    
-    return spks, correct.astype(bool)
-
-
 def fit_pca_model(spks,n_comp):
     
     import numpy as np
@@ -76,6 +19,7 @@ def fit_pca_model(spks,n_comp):
     model = PCA(n_components = n_comp).fit(droll.T)
     
     return model
+
     
     
 def compute_pca(spks, model, n_comp):
@@ -90,5 +34,28 @@ def compute_pca(spks, model, n_comp):
     pc = np.reshape(pc, (n_comp, -1, NB))
     
     return pc
+    
+    
+def plot_explained_variance(model, threshold = 0.9):
+    
+    from matplotlib import pyplot as plt
+    import numpy as np
+    
+    cum_exp_var = np.cumsum(model.explained_variance_ratio_)
+    
+    ncomp_thrsh = np.where(cum_exp_var > threshold)[0]
+    
+    plt.figure(figsize = (7,6))
+    ax = plt.subplot(111)
+    plt.plot(np.cumsum(model.explained_variance_ratio_))
+    
+    if len(ncomp_thrsh) > 0:
+        plt.hlines(threshold, 0, ncomp_thrsh[0], color = 'k', linestyle = '--')
+        plt.vlines(ncomp_thrsh[0], 0, threshold, color = 'k', linestyle = '--')
+    
+    ax.set(xlabel = 'Number of components', ylabel = 'Cumulative Explained Variance [%]')
+    plt.grid(True)
+    
+    
     
     
